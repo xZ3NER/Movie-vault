33077945393eb81ec65813b5fb57515cc353365e
Peng You <ypenggg11@gmail.com>
2023-03-05 12:20:45 +0000

    Header svg files added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import './App.css';
+import React from "react";
 
-function App() {
+import "./App.css";
+import Logo from "./components/Icons/Logo";
+import Menu from "./components/Icons/Menu";
+import User from "./components/Icons/User";
+
+import styles from "./components/Header/Header.module.css";
+
+const App = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <React.Fragment>
+      <header className={styles.header}>
+        <div className={styles.menu}>
+          <Menu fill="#fff" height='5em' width='5em' />
+          <Logo height='5em' width='5em' />
+        </div>
+        <div className={styles.user}>
+          <div className={styles.auth}>
+            <h2>Username</h2>
+            <p>Login/Register</p>
+          </div>
+          <User fill="#fff" height='5em' width='5em' />
+        </div>
+      </header>
+      <main>
+        <p>sss</p>
+      </main>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
